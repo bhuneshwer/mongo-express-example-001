703956ba4421467a9b88a5470eb162de0a1b8eb6
Bhunesh <bhuneshwer.jaiswal@litmusworld.com>
2020-06-20 11:10:51 +0530

    Implemented post and query param

--- server.js
+++ server.js
@@ -1,5 +1,12 @@
 const app = require('express')();
 const http = require('http').Server(app);
+var bodyParser = require('body-parser')
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+ 
+// parse application/json
+app.use(bodyParser.json())
 
 function getDbClient() {
     return new Promise((resolve, reject) => {
@@ -8,7 +15,6 @@ function getDbClient() {
             "useNewUrlParser": true,
             "useUnifiedTopology": true
         }).then(function(client) {
-            console.warn("client", client)
             resolve(client.db());
         }).catch(function(err) {
             reject(err);
@@ -16,14 +22,56 @@ function getDbClient() {
     });
 }
 
-app.get("/:username", (req, res) => {
-    var username = req.params.username;
+// Inserting data into database
+
+app.post("/student",function(req,res){
+    var rqstBody = req.body;
+    if(rqstBody && rqstBody.rollNo && req.body.name){
+        getDbClient().then((client) => {
+            var collection = client.collection('collection1');
+            var dataToInsert = {
+                "name":rqstBody.name,
+                "rollNo":rqstBody.rollNo
+            };
+            collection.insert(dataToInsert,(err,dataToInsert) => {
+                if(!err){
+                    res.send(dataToInsert)
+                }else{
+                    res.send(err.toString())
+                }
+            })    
+        }, (err) => {
+            res.send("Err while connecting to db err is ",err.toString())
+        })
+    }else{
+        res.send("Roll No and Name is mandatory field")
+    }
+})
+
+// Query Parameters example
+app.get("/", (req, res) => {
+    console.log( req.query)
+    // Trying to read data through  query param
+    var rollNo = req.query.rollNo;
+    var name = req.query.studentName;
     getDbClient().then((client) => {
         var collection = client.collection('collection1');
-        var cursor = collection.find({name:username});
+        var query = {};
+
+        // checking if input is number and converting it to int
+        if(rollNo && !isNaN(rollNo)){
+            rollNo = parseInt(rollNo)
+            query.rollNo = rollNo;
+        }
+        if(name && name.length){
+            query.name = name
+        }
+        
+        cursor = collection.find(query);
+
         cursor.toArray((err, results) => {
             if (!err) {
-                res.json({ "results": results,"username":username })
+                res.json({ "results": results})
             } else {
                 res.send({ "results": null })
             }
@@ -33,6 +81,28 @@ app.get("/:username", (req, res) => {
         res.send({ "results": err.toString() })
     })
 })
+
+
+
+// RouteParam example
+    
+// app.get("/:rollNo", (req, res) => {
+//     var rollNo = req.params.rollNo;
+//     getDbClient().then((client) => {
+//         var collection = client.collection('collection1');
+//         var cursor = collection.find({"rollNo":parseInt(rollNo)});
+//         cursor.toArray((err, results) => {
+//             if (!err) {
+//                 res.json({ "results": results })
+//             } else {
+//                 res.send({ "results": null })
+//             }
+//         });
+
+//     }, (err) => {
+//         res.send({ "results": err.toString() })
+//     })
+// })
 
 function startServer() {
     http.listen(3001, function() {
